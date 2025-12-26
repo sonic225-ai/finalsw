1fea3ac38199a074832e45874c9c60eb8e77bf16
aida529 <aida.bekenova@narxoz.kz>
2025-12-26 16:39:41 +0500

    Add repository

---
+++ src/main/java/Repository/DishRepository.java
@@ -0,0 +1,9 @@
+package Repository;
+
+import entity.DishEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DishRepository extends JpaRepository<DishEntity , Long> {
+}
