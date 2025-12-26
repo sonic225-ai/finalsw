f6d3b84df4d58f4a6b65a79eb0f61d97c53855e1
aida529 <aida.bekenova@narxoz.kz>
2025-12-26 15:32:43 +0500

    Initial commit

---
+++ src/main/java/com/example/aida/AidaApplication.java
@@ -0,0 +1,13 @@
+package com.example.aida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aida/AidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
